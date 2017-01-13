f78578739d140b9c32c4abfcf99b2c4030f7ab3c
Anne Belka <antastic461@gmail.com>
2017-01-13 09:35:02 -0800

    add save functionality

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -5,6 +5,15 @@ export default Ember.Component.extend({
   actions: {
     questionFormShow() {
       this.set('addNewQuestion', true);
+    },
+    saveQuestion() {
+      var params = {
+        content: this.get('content') ? this.get('content') : "",
+        author: this.get('author') ? this.get('author') : "",
+        notes: this.get('notes') ? this.get('notes') : "",
+      };
+    this.set('addNewQuestion', false);
+    this.sendAction('saveQuestion', params);
     }
   }
 });
